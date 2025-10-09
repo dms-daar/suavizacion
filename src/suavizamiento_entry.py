e912554070bc6a387d6c1e8751f570b88736c8a0
Diego Alvarado <diego.alvarado@dataminesoftware.com>
2025-10-09 18:23:42 -0500

    modified src/suavizamiento_entry.py to work with the find_max_cubic_tile_size function and tieded up final section

--- src/suavizamiento_entry.py
+++ src/suavizamiento_entry.py
@@ -117,35 +117,33 @@ elif version == 7:
     dists = [int(d) for d in args["dist"].split(",")]
     out_cols = [f"{out_col}_{d}" for d in dists]
 
-    max_dist = 50 # 1*max(dists)
+    max_dist = int(max(dists))
+    size = max_dist
+
+    try: 
+        size, stats = find_max_cubic_tile_size(
+            df=df,
+            max_dist=max_dist,
+            limit_indexed=50_000,
+            summarize_fn=summarize_bands_xyz_lean,
+            verbose=False
+        )
+        size = int(size)
+    
+    except Exception as e: 
+        log("Error when optimizing tile size. Falling back to max_dist")
+        size = max_dist
+
+    log(f"Chosen cubic tile size: {size}")
 
     suavizar_batched_xyz_multi_stable(
         df, col, dists, out_cols, 
-        x_size=max_dist, y_size=max_dist, z_size=max_dist,
+        x_size=size, y_size=size, z_size=size,
         log=log
     )
 
 
-# reports: mapping of report names to its json table
-response["reports"] = dict()
-
-
-# get the report of volume changes
-tvr, tvrf = reportar_volumenes(df, col, dists, out_cols)
-response["reports"]["TOTAL VOLUMES"] = df_to_json_table(tvrf)
-save_df_as_table(tvr, f"{bm}_total_volumnes", oDmApp)
-
-
-# for each distance, get the orig vs suav_dist report table
-for out_col, dist in zip(out_cols, dists):
-    report_name = f"VOLUME ORIG vs {dist}"
-    report =  report_volume_variation(df, "VOL", col, out_col)
-    response["reports"][report_name] = df_to_json_table(report)
-    save_df_as_table(report, f"{bm}_volume_orig_vs_{dist}", oDmApp)
-
-
-# construct a list with all the available reports
-response["reports_list"] = list(response["reports"].keys())
+#####################################################
 
 msg = f"Writing output"
 oDmApp.ControlBars.Output.write(msg)
@@ -163,6 +161,29 @@ oDmApp.parseCommand(command)
 
 command = "DELETE &IN=xxx2"
 oDmApp.parseCommand(command)
+
+#####################################################
+
+# reports: mapping of report names to its json table
+response["reports"] = dict()
+
+
+# get the report of volume changes
+tvr, tvrf = reportar_volumenes(df, col, dists, out_cols)
+response["reports"]["TOTAL VOLUMES"] = df_to_json_table(tvrf)
+save_df_as_table(tvr, f"{bm}_total_volumes", oDmApp)
+
+
+# for each distance, get the orig vs suav_dist report table
+for out_col, dist in zip(out_cols, dists):
+    report_name = f"VOLUME ORIG vs {dist}"
+    cvr, cvrf =  report_volume_variation(df, "VOL", col, out_col)
+    response["reports"][report_name] = df_to_json_table(cvrf)
+    save_df_as_table(cvr, f"{bm}_volume_orig_vs_{dist}", oDmApp)
+
+
+# construct a list with all the available reports
+response["reports_list"] = list(response["reports"].keys())
 
 #####################################################
 
