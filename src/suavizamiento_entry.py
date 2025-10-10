cca4fb2e9b35c933900e7a9b6c8f1e8be827b3a4
Diego Alvarado <diego.alvarado@dataminesoftware.com>
2025-10-10 09:44:15 -0500

    organized and removed old code from src/suavizamiento_entry.py

--- src/suavizamiento_entry.py
+++ src/suavizamiento_entry.py
@@ -2,16 +2,12 @@
 
 import pandas as pd 
 import numpy as np
-from matplotlib import pyplot as plt 
-import seaborn as sns
-from scipy.spatial import cKDTree 
-
 import json 
 import sys 
-
 from rmprocs.dm import *
 from rmprocs.suavizamiento import *
 
+#####################################################
 
 oDmApp, PROJECT_FOLDER = get_oDmApp()
 
@@ -29,150 +25,63 @@ col = args["col"]
 out_bm = args["out_bm"]
 out_col = args["out_col"]
 
-
 response = dict()
 
 #####################################################
 
 log(f"Suavizamiento V{version}\n")
 
-# read the block model
-command = f"""
-SELCOP 
-&IN={bm} &OUT=xxxBMF 
-*F1=XC *F2=YC *F3=ZC 
-*F4=XINC *F5=YINC *F6=ZINC 
-*F7={col}
-""".replace("\n", " ")
-
-oDmApp.parseCommand(command)
-
-df = get_dm_table("xxxBMF", oDmApp)
-
-command = "DELETE &IN=xxxBMF"
-oDmApp.parseCommand(command)
-
+columns = ["XC", "YC", "ZC", "XINC", "YINC", "ZINC", col]
+df = get_dm_table(bm, oDmApp, columns=columns)
 df[["XC", "YC", "ZC"]] = df[["XC", "YC", "ZC"]].astype("float32")
 df["VOL"] = df["XINC"] * df["YINC"] * df["ZINC"] 
 
+#####################################################
 
-if version == 1: 
+dists = [int(d) for d in args["dist"].split(",")]
+out_cols = [f"{out_col}_{d}" for d in dists]
 
-    dist = int(args["dist"])
-    out_cols = [out_col]
+max_dist = int(max(dists))
+size = max_dist
 
-    suavizar_col(df, col, dist, out_col)
-
-elif version == 2: 
-
-    dists = [int(d) for d in args["dist"].split(",")]
-    out_cols = [f"{out_col}_{d}" for d in dists]
-
-    suavizar_multiple(df, col, dists, out_cols)
-
-elif version == 3: 
-
-    dist = int(args["dist"])
-    out_cols = [out_col]
-
-    suavizar_col_batched(
-        df, col, dist, out_col, 
-        log=log
+try: 
+    size, stats = find_max_cubic_tile_size(
+        df=df,
+        max_dist=max_dist,
+        limit_indexed=50_000,
+        summarize_fn=summarize_bands_xyz_lean,
+        verbose=False
     )
+    size = int(size)
 
-elif version == 4: 
-
-    dists = [int(d) for d in args["dist"].split(",")]
-    out_cols = [f"{out_col}_{d}" for d in dists]
-
-    suavizar_batched_multi(
-        df, col, dists, out_cols, 
-        log=log
-    )
-
-elif version == 5: 
-
-    dist = int(args["dist"])
-    out_cols = [out_col]
-
-    suavizar_col_batched_xyz(
-        df, col, dist, out_col, 
-        x_size=100, y_size=100, z_size=50,
-        log=log
-    )
-
-elif version == 6: 
-
-    dists = [int(d) for d in args["dist"].split(",")]
-    out_cols = [f"{out_col}_{d}" for d in dists]
-
-    suavizar_batched_xyz_multi(
-        df, col, dists, out_cols, 
-        x_size=50, y_size=50, z_size=50,
-        log=log
-    )
-
-elif version == 7: 
-
-    dists = [int(d) for d in args["dist"].split(",")]
-    out_cols = [f"{out_col}_{d}" for d in dists]
-
-    max_dist = int(max(dists))
+except Exception as e: 
+    log("Error when optimizing tile size. Falling back to max_dist")
     size = max_dist
 
-    try: 
-        size, stats = find_max_cubic_tile_size(
-            df=df,
-            max_dist=max_dist,
-            limit_indexed=50_000,
-            summarize_fn=summarize_bands_xyz_lean,
-            verbose=False
-        )
-        size = int(size)
-    
-    except Exception as e: 
-        log("Error when optimizing tile size. Falling back to max_dist")
-        size = max_dist
+log(f"Chosen cubic tile size: {size}")
 
-    log(f"Chosen cubic tile size: {size}")
-
-    suavizar_batched_xyz_multi_stable(
-        df, col, dists, out_cols, 
-        x_size=size, y_size=size, z_size=size,
-        log=log
-    )
-
+suavizar_batched_xyz_multi_stable(
+    df, col, dists, out_cols, 
+    x_size=size, y_size=size, z_size=size,
+    log=log
+)
 
 #####################################################
 
 msg = f"Writing output"
 oDmApp.ControlBars.Output.write(msg)
 
-command = f"EXTRA &IN={bm} &OUT=xxx1 'GO'"
-oDmApp.parseCommand(command)
-
-save_df_as_table(df[out_cols], "xxx2", oDmApp)
-
-command = f"SPLAT &IN1=xxx1 &IN2=xxx2 &out={out_bm}"
-oDmApp.parseCommand(command)
-
-command = "DELETE &IN=xxx1"
-oDmApp.parseCommand(command)
-
-command = "DELETE &IN=xxx2"
-oDmApp.parseCommand(command)
+add_cols(bm, df[out_cols], out_bm, oDmApp)
 
 #####################################################
 
 # reports: mapping of report names to its json table
 response["reports"] = dict()
 
-
 # get the report of volume changes
 tvr, tvrf = reportar_volumenes(df, col, dists, out_cols)
 response["reports"]["TOTAL VOLUMES"] = df_to_json_table(tvrf)
 save_df_as_table(tvr, f"{bm}_total_volumes", oDmApp)
-
 
 # for each distance, get the orig vs suav_dist report table
 for out_col, dist in zip(out_cols, dists):
@@ -181,7 +90,6 @@ for out_col, dist in zip(out_cols, dists):
     response["reports"][report_name] = df_to_json_table(cvrf)
     save_df_as_table(cvr, f"{bm}_volume_orig_vs_{dist}", oDmApp)
 
-
 # construct a list with all the available reports
 response["reports_list"] = list(response["reports"].keys())
 
@@ -189,5 +97,4 @@ response["reports_list"] = list(response["reports"].keys())
 
 response["status"] = "OK"
 response = json.dumps(response)
-
 print(response)
